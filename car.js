e5acb222201d1e748e5a7ec45e2f7fc7a0763c89
MUBASHIRKHAN-PROGRAMMER <mk765pc@gmail.com>
2025-06-06 17:00:04 +0500

    color changer

--- car.js
+++ car.js
@@ -1,21 +1,21 @@
 let button = document.querySelector('button');
 let carimage = document.querySelector('.carbody');
 
-carimage.setAttribute("class" , "carbody");
-
 button.addEventListener('click', () => {
 
-const red = Math.floor(Math.random() * 256);
-const green =  Math.floor(Math.random() * 256);
-const blue =  Math.floor(Math.random() * 256);
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
 
-const rgb =  `rgb(${red} , ${green} , ${blue})`;
-carimage.style.background = rgb ;
+    const rgb = `rgb(${red} , ${green} , ${blue})`;
+    carimage.style.background = rgb;
 
-carimage.setAttribute("class" , "carbodyVisible");
+    // carimage.style.display = 'none';
+   
 
-const span = document.querySelector('span');
-span.innerHTML = ` rgb(${red} , ${green} , ${blue}) `;
+
+    const span = document.querySelector('span');
+    span.innerHTML = ` rgb(${red} , ${green} , ${blue}) `;
 
 });
 
